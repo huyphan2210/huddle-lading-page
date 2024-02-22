f98e0c981c31e3e321cffbed86397b89fb654c19
Huy Phan <huy.phan2210@outlook.com>
2024-02-22 09:33:40 +0700

    Finish styling HeroSection

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import "./Main.scss";
 import HeroSection from "./components/HeroSection/HeroSection";
+import FeatureSection from "./components/FeatureSection/FeatureSection";
 
 const Main: FC = () => {
   return (
     <main>
       <HeroSection />
+      <FeatureSection />
     </main>
   );
 };
