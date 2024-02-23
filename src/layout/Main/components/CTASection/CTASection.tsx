5456ab2f0fcd746d1df9c5e03496b406fc182bc2
Huy Phan <huy.phan2210@outlook.com>
2024-02-23 11:36:36 +0700

    Create CTA

---
+++ src/layout/Main/components/CTASection/CTASection.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import "./CTASection.scss";
+
+const CTASection: FC = () => {
+  return (
+    <section className="cta-section">
+      <h1>Ready To Build Your Community?</h1>
+      <button type="button">Get Started For Free</button>
+    </section>
+  );
+};
+
+export default CTASection;
