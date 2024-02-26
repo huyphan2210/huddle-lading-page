5b2761131c8f2c5c3e6ac9639fb080c89625090b
Huy Phan <huy.phan2210@outlook.com>
2024-02-26 09:07:07 +0700

    Create layout for footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,10 +1,17 @@
 import { FC } from "react";
 import "./Footer.scss";
+import footerLogo from "../../assets/images/logo-cyan.svg";
 
 const Footer: FC = () => {
-  return <footer>
-    
-  </footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__logo">
+        <img src={footerLogo} loading="lazy" title="Footer Logo"/>
+      </div>
+      <div className="footer__content"></div>
+      <div className="footer__copyright"></div>
+    </footer>
+  );
 };
 
 export default Footer;
